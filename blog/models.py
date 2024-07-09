59e20f1fcc3878e4e15aff3a83977571cd41b1c8
Alex Alex <alexthevries@gmail.com>
2024-07-09 14:43:23 +0000

    finish crispy forms

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-
 class Tag(models.Model):
     value = models.TextField(max_length=100)
 
@@ -21,3 +20,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
