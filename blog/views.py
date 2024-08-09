4ed3e7d51aeaf5ed487ec47a2a642b17ddc2fb43
Alex Alex <alexthevries@gmail.com>
2024-08-09 09:38:06 +0000

    finish database optimize

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,11 @@ def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
-    
+
+def get_ip(request):
+  from django.http import HttpResponse
+  return HttpResponse(request.META['REMOTE_ADDR'])
+      
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
 
